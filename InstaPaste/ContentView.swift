b929b49cf66708003900073a8514c5fca2075f1c
AdityaSurpur <surpur.aditya@gmail.com>
2026-05-02 21:52:59 +0200

    Initial Commit

---
+++ InstaPaste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstaPaste
+//
+//  Created by Aditya Surpur on 02.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstaPaste/InstaPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaPasteApp.swift
+//  InstaPaste
+//
+//  Created by Aditya Surpur on 02.05.26.
+//
+
+import SwiftUI
+
+@main
+struct InstaPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
